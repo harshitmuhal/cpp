6cfb7fe649ad12cc1cf1945a2a5aea7f7a93e0ce
harshit muhal <hrshtmuhal8@gmail.com>
2019-07-11 19:33:52 +0530

    new change

---
+++ data structures/graph.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<list>
+#include<queue>
+#include<map>
+using namespace std;
+
+template<typename t>
+class graph{
+public:
+    int v;
+    map<t,list<t> > adjlist;
+    
+    graph(int d){
+        v=d;
+    }
+    
+    void addedge(t i,t j,bool isbidirectional=true){
+        adjlist[i].push_back(j);
+        if(isbidirectional){
+            adjlist[j].push_back(i);
+        }
+    }
+};
