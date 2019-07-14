824521c0c258529d211f4e9716cb0a3245aee0b3
harshit muhal <hrshtmuhal8@gmail.com>
2019-07-14 10:43:25 +0530

    change

---
+++ primeSieve.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+void primeSieve(int *p,long long int n){
+    
+    p[0] = p[1] = 0;
+    p[2] = 1;
+    
+    
+    for(long long int i=3;i<=n;i+=2){
+        p[i] = 1;
+    }
+    
+    for(long long int i=3;i<=n;i+=2){
+        
+        if(p[i]){
+            for(long long int j=i*i;j<=n;j+=2*i){
+                p[j] = 0;
+            }
+        }
+        
+    }
+    
+    return;
+}
+
+int main() {
+    
+    int p[1000001] = {0};
+    int t;
+    cin>>t;
+    primeSieve(p,t);
+    
+    return 0;       
+}
